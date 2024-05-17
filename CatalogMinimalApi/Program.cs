d91eb7dd7b1afcd83d125f06dc21fcecd96d0e5c
Giovani de Oliveira <giovani98@proton.me>
2024-05-17 13:57:55 -0300

    refact: Refactor Program.cs

--- CatalogMinimalApi/Program.cs
+++ CatalogMinimalApi/Program.cs
@@ -1,3 +1,4 @@
+using CatalogMinimalApi.AppServicesExtensions;
 using CatalogMinimalApi.Context;
 using CatalogMinimalApi.Endpoints;
 using CatalogMinimalApi.Models;
@@ -9,75 +10,16 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
 
-#region Configuration and build
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CatalogMinimalApi", Version = "v1" });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme."
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                          new OpenApiSecurityScheme
-                          {
-                              Reference = new OpenApiReference
-                              {
-                                  Type = ReferenceType.SecurityScheme,
-                                  Id = "Bearer"
-                              }
-                          },
-                         new string[] {}
-                    }
-                });
-});
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
-);
-
-// Registra serviço de token
-builder.Services.AddSingleton<ITokenService>(new TokenService());
-
-// Serviço de autenticação
-builder.Services.AddAuthentication
-                 (JwtBearerDefaults.AuthenticationScheme)
-                 .AddJwtBearer(options =>
-                 {
-                     options.TokenValidationParameters = new TokenValidationParameters
-                     {
-                         ValidateIssuer = true,
-                         ValidateAudience = true,
-                         ValidateLifetime = true,
-                         ValidateIssuerSigningKey = true,
-
-                         ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                         ValidAudience = builder.Configuration["Jwt:Audience"],
-                         IssuerSigningKey = new SymmetricSecurityKey
-                         (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-                     };
-                 });
-
-builder.Services.AddAuthorization();
+builder.AddApiSwagger();
+builder.AddPersistence();
+builder.Services.AddCors();
+builder.AddJwtAuthentication();
 
 var app = builder.Build();
-#endregion
 
-// /////////////////////// Endpoints ///////////////////////
+#region API Endpoints
 
 app.MapAuthenticationEndpoints();
 
@@ -85,14 +27,13 @@ app.MapCategoryEndpoints();
 
 app.MapProductEndpoints();
 
-// /////////////////////////////////////////////////////////
+#endregion
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+var environment = app.Environment;
+
+app.UseExceptionHandling(environment)
+    .UseSwaggerMiddleware()
+    .UseAppCors();
 
 // Ativa autenticação e autorização
 app.UseAuthentication();
