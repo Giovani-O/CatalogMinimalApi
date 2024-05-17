a81769ed8195770902ca2f12a86fd8ae65994d97
Giovani de Oliveira <giovani98@proton.me>
2024-05-17 10:24:51 -0300

    feat: Create category endpoints

--- CatalogMinimalApi/Program.cs
+++ CatalogMinimalApi/Program.cs
@@ -1,4 +1,5 @@
 using CatalogMinimalApi.Context;
+using CatalogMinimalApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,54 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 );
 
 var app = builder.Build();
+
+// /////////////////////// Endpoints ///////////////////////
+
+app.MapGet("/categories", async (AppDbContext db) => await db.Categories.ToListAsync());
+
+app.MapGet("/categories/{id:int}", async (int id, AppDbContext db) =>
+{
+    return await db.Categories.FindAsync(id) is Category category
+        ? Results.Ok(category)
+        : Results.NotFound();
+});
+
+app.MapPost("/categories", async (Category category, AppDbContext db) => 
+{ 
+    db.Categories.Add(category);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/categories/{category.Id}", category);
+});
+
+app.MapPut("/categories/{id:int}", async (int id, Category updatedCategory, AppDbContext db) =>
+{
+    if (updatedCategory.Id != id)
+        return Results.BadRequest();
+
+    var category = await db.Categories.FindAsync(id);
+
+    if (category is null) return Results.NotFound();
+
+    category.Name = updatedCategory.Name;
+    category.Description = updatedCategory.Description;
+
+    await db.SaveChangesAsync();
+    return Results.Ok(category);
+});
+
+app.MapDelete("/categories/{id:int}", async (int id, AppDbContext db) => 
+{
+    var category = await db.Categories.FindAsync(id);
+
+    if (category is null) return Results.NotFound();
+
+    db.Categories.Remove(category);
+    await db.SaveChangesAsync();
+    return Results.Ok(category);
+});
+
+// /////////////////////////////////////////////////////////
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
